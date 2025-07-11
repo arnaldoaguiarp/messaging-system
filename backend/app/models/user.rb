5bbb845e5ea4b0a4a959b6adc199690e512eedb8
arnaldoap <arnaldoaguiarp@gmail.com>
2025-07-11 09:54:20 -0300

    feat: add Devise for user authentication

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :validatable, :jwt_authenticatable,
+         jwt_revocation_strategy: self
+
   belongs_to :role
 
   has_many :sent_messages, class_name: "Message", foreign_key: "sender_id", dependent: :destroy
